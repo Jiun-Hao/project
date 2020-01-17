fdc601fc6dd8903272416c9970a3df9f765d16bf
Jiun-Hao <34904513@qq.com>
2020-01-17 15:14:14 +0800

    更新登陆页面

--- src/main/java/com/zhuzhu/project/api/controller/Test.java
+++ src/main/java/com/zhuzhu/project/api/controller/Test.java
@@ -2,6 +2,7 @@ package com.zhuzhu.project.api.controller;
 
 import com.zhuzhu.project.api.model.User;
 import com.zhuzhu.project.api.service.impl.UserLogInServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.logging.stdout.StdOutImpl;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @Controller
+@Slf4j
 @RequestMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class Test {
     @Resource
@@ -25,7 +29,7 @@ public class Test {
 
     @PostMapping("/login")
     @ResponseBody
-    public String login(@RequestBody User user, HttpServletRequest request){
+    public String login(@RequestBody User user, HttpServletRequest request, HttpServletResponse response){
         System.out.println("id:"+user.getUserSerialId());
         System.out.println("pass:"+user.getUserPassword());
         if(userLogInService.login(user)){
