39c460c138ca114a2ead1897a39380ca8d41d4f0
Jiun-Hao <34904513@qq.com>
2020-01-19 11:17:01 +0800

    更新表单重复提交

--- src/main/java/com/zhuzhu/project/api/controller/PostController.java
+++ src/main/java/com/zhuzhu/project/api/controller/PostController.java
@@ -1,4 +1,34 @@
 package com.zhuzhu.project.api.controller;
 
+import com.zhuzhu.project.api.model.User;
+import com.zhuzhu.project.api.service.impl.UserLogInServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+@RequestMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class PostController {
+
+    @Resource
+    private UserLogInServiceImpl userLogInService;
+    @PostMapping("/login")
+    public String login(User user, HttpServletRequest request, HttpServletResponse response) throws Exception{
+        request.setAttribute("userSerialId",user.getUserSerialId());
+        if(userLogInService.login(user)){
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            return "redirect:/main/main.html";
+        }
+        request.setAttribute("LoginRes", "账号或密码错误");
+        return "login";
+    }
 }
--- src/main/java/com/zhuzhu/project/api/controller/StaticController.java
+++ src/main/java/com/zhuzhu/project/api/controller/StaticController.java
@@ -1,4 +1,22 @@
 package com.zhuzhu.project.api.controller;
 
+import com.zhuzhu.project.api.service.impl.UserLogInServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.annotation.Resource;
+
+@Controller
+@Slf4j
+@RequestMapping(value = "/", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class StaticController {
+
+    @Resource
+    private UserLogInServiceImpl userLogInService;
+
+    @RequestMapping("/login")
+    public String test(){
+        return "redirect:/login/login.html";
+    }
 }
